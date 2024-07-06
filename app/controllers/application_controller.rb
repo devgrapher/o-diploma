3caab618b42efd5f653056d3fb78ce39d7e1db40
Jihun Kim <devgrapher@gmail.com>
2024-07-06 22:48:52 +0900

    og

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   before_action :build_meta_tags
 
   def build_meta_tags
-    set_meta_tags description: "완주를 축하합니다", og: { title: '플레이오그라운드', site_name: '플레이오그라운드', description: '기록 확인', image: 'https://diploma.orienteering.co.kr/thumb.png?v1' }
+    set_meta_tags description: "기록증 확인", og: { title: '오리엔티어링', site_name: '오리엔티어링', description: '기록증 확인', image: 'https://odiploma.s3.ap-northeast-2.amazonaws.com/main.jpeg?v1' }
   end
 end
